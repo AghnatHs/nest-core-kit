ca0f59a2954096d7d0bdd9cc028fd2fb796c5a83
AghnatHs <aghnathasya@gmail.com>
2025-07-15 21:48:21 +0700

    Feat(main): init)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { initializeApp } from './app';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  initializeApp(app);
+
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 void bootstrap();
