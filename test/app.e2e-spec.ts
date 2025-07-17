a519b643a13afb4874a31e2472109b4ad96b82fa
AghnatHs <aghnathasya@gmail.com>
2025-07-17 18:32:58 +0700

    Fix(app.e2e): app didnt close after testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,6 +18,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer()).get('/').expect(404);
   });
