527dd310506f8511e762dee4c3814e440af7f44a
AghnatHs <aghnathasya@gmail.com>
2026-01-30 19:38:27 +0700

    feat(pagination): remove own pagination, use nestjs-paginate instead

--- src/core/pagination/pagination.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CursorPaginationProvider } from './cursor-pagination/cursor-pagination';
-import { OffsetPaginationProvider } from './offset-pagination/offset-pagination';
-
-@Module({
-  providers: [CursorPaginationProvider, OffsetPaginationProvider],
-  exports: [CursorPaginationProvider, OffsetPaginationProvider],
-})
-export class PaginationModule {}
